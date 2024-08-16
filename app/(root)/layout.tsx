775773e2fed7bec6327ec20a84b6ab7d6fc9ed8b
gileludin <gileludin@gmail.com>
2024-08-16 10:34:33 +0700

    Implement auth, appwrite, and sentry

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,27 +1,30 @@
 import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import { getLoggedInUser } from "@/lib/actions/user.action";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = { firstName: 'Gileludin', lastName: 'TEST' };
+  const loggedIn = await getLoggedInUser();
+
+  if (!loggedIn) redirect("/sign-in");
 
   return (
     <main className="flex h-screen w-full font-inter">
-        <Sidebar user={loggedIn} />
-        <div className="flex size-full flex-col">
-          <div className="root-layout">
-            <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
-              <div>
-                  <MobileNav user={loggedIn} />
-              </div>
+      <Sidebar user={loggedIn} />
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
           </div>
-        {children}
         </div>
+        {children}
+      </div>
     </main>
-
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,34 +1,35 @@
-import HeaderBox from '@/components/HeaderBox'
-import RightSideBar from '@/components/RightSideBar';
-import TotalBalanceBox from '@/components/TotalBalanceBox';
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getLoggedInUser } from "@/lib/actions/user.action";
 
-const Home = () => {
-  const loggedIn = { firstName: 'Gileludin', lastName: 'Test', email: 'Gileludin@gmail.com'};
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
   return (
     <section className="home">
-        <div className="home-content">
-            <header className="home-header">
-                <HeaderBox
-                    type="greeting"
-                    title="Welcome"
-                    user={loggedIn?.firstName || 'Guest'}
-                    subtext="Access and manage your account and transactions efficiently."
-                />
-                <TotalBalanceBox
-                    accounts={[]}
-                    totalBanks={1}
-                    totalCurrentBalance={1250.35}
-                />
-            </header>
-            RECENT TRANSACTION
-        </div>
-        <RightSideBar 
-            user={loggedIn}
-            transactions={[]}
-            banks={[{ currentBalance: 123.50}, {currentBalance: 123.50}]}
-        />
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.name || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 123.5 }]}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
